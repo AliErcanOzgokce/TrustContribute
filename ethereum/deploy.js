4557d60a618c00e06c73b102de481b8c01f74ab5
AliErcanOzgokce <78970916+AliErcanOzgokce@users.noreply.github.com>
2022-04-07 18:24:05 +0300

    Update deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,9 +3,9 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-  'flip reason worth risk attend swift absorb broken cup gym similar pet',
+  'REPLACE_WITH_YOUR_MNEMONIC',
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
+  'REPLACE_WITH_YOUR_HTTP_PROVIDER'
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
