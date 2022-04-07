6a9994af19794ed2aba3061f1ad9a8c9a52b7389
AliErcanOzgokce <78970916+AliErcanOzgokce@users.noreply.github.com>
2022-04-07 18:22:44 +0300

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x4f32efD59fC3E01926b170EAB154a80Ef7efbd6c'
+  'CHANGE HERE TO YOUR CONTRACT ADDRESS'
 );
 
 export default instance;
